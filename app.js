706633c083ca8213eb6dc68aeb662b76bae5ba94
Antonio Stassi <stassiant@outlook.it>
2024-08-30 17:06:10 +0200

    Some style added + adding task logic

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ const { createApp } = Vue
             { task: "Go to the toilet", done: false},
             { task: "Turning on PC", done: false},
             { task: "Start coding", done: false},
-        ]
+        ],
+        taskToAdd: ""
       }
+    },
+    methods: {
+        addNewTask(){
+            const newTask = this.taskToAdd.trim();
+            const newObj = {task: newTask, done: false};
+            if(newTask != "") {
+                this.todoList.push(newObj);
+                this.taskToAdd = "";
+            }
+        }
     }
   }).mount('#app')
