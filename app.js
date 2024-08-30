04be31da2d91e80d2efb4ad748f114229f7ed996
Antonio Stassi <stassiant@outlook.it>
2024-08-30 16:39:42 +0200

    HTML & JS | List + Button to remove list item

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Loaded");
+
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        todoList: [
+            { task: "Waking up", done: false},
+            { task: "Eat", done: false},
+            { task: "Drink water", done: false},
+            { task: "Go to the toilet", done: false},
+            { task: "Turning on PC", done: false},
+            { task: "Start coding", done: false},
+        ]
+      }
+    }
+  }).mount('#app')
